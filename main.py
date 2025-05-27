67efe2b5099dec2abc7beefbc91b3e0f0d9c2b3c
Anders-Enrico <s224076@dtu.dk>
2025-05-27 11:34:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ INTERVAL_SECONDS = 0.1
 MAX_DURATION_SECONDS = 0
 SHUTDOWN_PIN = 20
 LED_PIN = 21
-USB_PATH = "/media/bird/LOGGER"
+USB_PATH = "/media/bird/LOGGER1"
 SERIAL_PORT = "/dev/ttyUSB0"
 
 def blink_led(blink_interval, stop_event):
